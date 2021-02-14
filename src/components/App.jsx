c8bccccc543b1c52a251e6bedcc636cfe76ed360
Donovan <donovanandrews300@gmail.com>
2021-02-14 11:22:48 -0500

    created lobby component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import 'assets/scss/App.scss';
-import AboutPage from 'components/AboutPage';
-import WelcomePage from 'components/WelcomePage';
+import Lobby from './Lobby'
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
+import '../styles/main.css'
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="app">
-          <nav>
-            <Link to="/">Home</Link> | <Link to="/about">About</Link>
-          </nav>
-          <hr />
-          <Route exact path="/" component={WelcomePage} />
-          <Route path="/about" component={AboutPage} />
-        </div>
-      </BrowserRouter>
+      <div>
+      <Lobby/>
+  </div>
+
     );
   }
 }
